691d969fd55558827718b0848bb107249f804a1d
Claire Davies <claire.davies@junitech.com>
2021-04-23 17:53:23 +0100

    got server working

--- server.js
+++ server.js
@@ -11,16 +11,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const databaseUrl = "workout";
-const collections = ["workouts"];
 
-const db = mongojs(databaseUrl, collections);
-
-db.on("error", error => {
-    console.log("Database Error:", error);
-});
-
-app.listen(3000, () => {
-    console.log("App running on port 3000!");
+app.listen(3001, () => {
+    console.log("App running on port 3001!");
 });
   
